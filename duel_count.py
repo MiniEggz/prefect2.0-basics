1769a9f8981f36c03d4aae7a7a9e9ffcf7660a3e
MiniEggz <owenscook1@gmail.com>
2022-06-28 16:38:11 +0100

    Added example of running tasks asynchronously

---
+++ duel_count.py
@@ -0,0 +1,25 @@
+from prefect import flow, task
+from prefect.task_runners import ConcurrentTaskRunner
+
+@task
+def count_one():
+    for i in range(1,1000000):
+        print(i)
+    return 42
+
+@task
+def count_two():
+    for i in range(-1000000,0):
+        print(i)
+    return 43
+
+@flow(task_runner=ConcurrentTaskRunner)
+def async_flow():
+    ans1 = count_one()
+    ans2 = count_two()
+    return
+
+async_flow()
+
+# when run can see that they are running together
+# blocks of +ve and -ve counts...
